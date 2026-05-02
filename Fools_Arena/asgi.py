80b93368435886706d128c23f1833cbf4af7ae8e
Maxim <96714576+Viton8@users.noreply.github.com>
2026-05-02 14:04:43 +0300

    Apply suggestions from code review
    Co-authored-by: Kiryl Alishkevich <64920776+uxabix@users.noreply.github.com>

--- Fools_Arena/asgi.py
+++ Fools_Arena/asgi.py
@@ -1,7 +1,7 @@
 """
 ASGI config for Fools_Arena project.
 
-It exposes the ASGI callable as a module-level variable namedя ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
